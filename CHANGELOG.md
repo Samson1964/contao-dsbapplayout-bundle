162c11259d67f8d94f169fb4679ffe965f8e99c1
Frank Hoppe <hoppe.berlin@googlemail.com>
2023-06-23 13:15:02 +0200

    * Ausgebaut zur funktionsfähigen Version. Der Cookiename fehlt aber noch.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Layout für DSB-App setzen für Contao Changelog
 
+## Version 0.0.2 (2023-06-23)
+
+* Ausgebaut zur funktionsfähigen Version. Der Cookiename fehlt aber noch.
+
 ## Version 0.0.1 (2023-06-23)
 
 * Initialversion als Contao-4-Bundle
--- src/Classes/AppLayout.php
+++ src/Classes/AppLayout.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2015 Leo Feyer
+ * Copyright (c) 2005-2023 Leo Feyer
  *
  * @license LGPL-3.0+
  */
@@ -11,46 +10,30 @@
 namespace Schachbulle\ContaoDsbapplayoutBundle\Classes;
 
 /**
- * Helper class for Ajax Layout selection
+ * Klasse für Ausgabe des Layouts für die DSB-App
  *
- * @author Fritz Michael Gschwantner <https://github.com/fritzmg>
+ * @author Fank Hoppe
  */
+
 class AppLayout
 {
 	public function getPageLayout(\PageModel $objPage, \LayoutModel &$objLayout, \PageRegular $objPageRegular)
 	{
-		// layout must use ajax layout
-		if( !$objLayout->useAjaxLayout )
+		if(!\Input::get('dsbapp'))
 			return;
 
-		// request must be ajax
-		if( !\Environment::get('isAjaxRequest') )
-			return;
+		// create default layout
+		$objAjaxLayout = new \LayoutModel();
+		$objAjaxLayout->pid = $objLayout->pid;
+		$objAjaxLayout->rows = '1rw';
+		$objAjaxLayout->cols = '1cl';
+		$objAjaxLayout->orderExt = '';
+		$objAjaxLayout->modules = 'a:1:{i:0;a:3:{s:3:"mod";s:1:"0";s:3:"col";s:4:"main";s:6:"enable";s:1:"1";}}';
+		$objAjaxLayout->template = 'fe_page_app';
+		$objAjaxLayout->doctype = $objLayout->doctype;
+		$objAjaxLayout->defaultImageDensities = $objLayout->defaultImageDensities;
 
-		// set custom or default ajax layout
-		if( $objLayout->customAjaxLayout )
-		{
-			// load custom layout
-			if( ( $objAjaxLayout = \LayoutModel::findById( $objLayout->customAjaxLayout ) ) !== null )
-			{
-				$objLayout = $objAjaxLayout;
-			}
-		}
-		else
-		{
-			// create default layout
-			$objAjaxLayout = new \LayoutModel();
-			$objAjaxLayout->pid = $objLayout->pid;
-			$objAjaxLayout->rows = '1rw';
-			$objAjaxLayout->cols = '1cl';
-			$objAjaxLayout->orderExt = '';
-			$objAjaxLayout->modules = 'a:1:{i:0;a:3:{s:3:"mod";s:1:"0";s:3:"col";s:4:"main";s:6:"enable";s:1:"1";}}';
-			$objAjaxLayout->template = 'fe_page_app';
-			$objAjaxLayout->doctype = $objLayout->doctype;
-			$objAjaxLayout->defaultImageDensities = $objLayout->defaultImageDensities;
-
-			$objLayout = $objAjaxLayout;
-		}
+		$objLayout = $objAjaxLayout;
 
 		// Do not cache
 		$objPage->cache = false;
